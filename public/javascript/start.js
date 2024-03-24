909da4381978b4cba1f64cab82eb480af48a5d30
Almeida <rodrigoduraoalmeida@gmail.com>
2024-03-24 14:44:16 +0000

    Página Inical em principio terminada

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     var button = document.getElementById('loginButton');
     var modal = document.getElementById('loginPopup');
     var closeModalButton = document.querySelector('.closeModal');
@@ -7,16 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     button.addEventListener('click', function () {
         modal.style.display = 'block';
+        // Cria o overlay
         overlay = document.createElement('div');
         overlay.classList.add('overlay');
         document.body.appendChild(overlay);
-        overlay.addEventListener('click', function () {
+        // Define a função para fechar o modal e remover o overlay
+        function closeModal() {
             modal.style.display = 'none';
             overlay.remove();
-        });
-    });
-    closeModalButton.addEventListener('click', function () {
-        modal.style.display = 'none';
-        overlay.remove(); 
+        }
+        // Adiciona evento de clique no overlay para fechar o modal
+        overlay.addEventListener('click', closeModal);
+        // Adiciona evento de clique no botão de fechar para fechar o modal
+        closeModalButton.addEventListener('click', closeModal);
     });
 });
