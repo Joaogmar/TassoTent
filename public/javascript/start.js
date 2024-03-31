cb30b85fd1527dd77064588f86fc9369ebfbd92d
afih <afonsotgoulart@gmail.com>
2024-03-31 20:32:00 +0100

    popup login fixed

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('loginButton');
-    var modal = document.getElementById('loginPopup');
-    var closeModalButton = document.querySelector('.closeModal');
-    var overlay;
+document.addEventListener("DOMContentLoaded", function() {
+    var loginButton = document.getElementById("loginButton");
+    var loginPopup = document.getElementById("loginPopup");
+    var closeModalButton = document.getElementById("closeModal");
+    var overlay = document.getElementById("overlay");
 
-    button.addEventListener('click', function () {
-        modal.style.display = 'block';
-        // Cria o overlay
-        overlay = document.createElement('div');
-        overlay.classList.add('overlay');
-        document.body.appendChild(overlay);
-        // Define a função para fechar o modal e remover o overlay
-        function closeModal() {
-            modal.style.display = 'none';
-            overlay.remove();
-        }
-        // Adiciona evento de clique no overlay para fechar o modal
-        overlay.addEventListener('click', closeModal);
-        // Adiciona evento de clique no botão de fechar para fechar o modal
-        closeModalButton.addEventListener('click', closeModal);
+    loginButton.addEventListener("click", function() {
+        // Exibe o popup de login
+        loginPopup.style.display = "block";
+        overlay.style.display = "block";
+    });
+
+    closeModalButton.addEventListener("click", function() {
+        // Esconde o popup de login
+        loginPopup.style.display = "none";
+        overlay.style.display = "none";
+    });
+
+    overlay.addEventListener("click", function() {
+        loginPopup.style.display = "none";
+        overlay.style.display = "none";
     });
 });
