df071681c3bd5b6f1a8bb3eec40ae65c9f724f3a
afih <afonsotgoulart@gmail.com>
2024-03-31 20:40:33 +0100

    transition popup login done

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -5,19 +5,34 @@ document.addEventListener("DOMContentLoaded", function() {
     var overlay = document.getElementById("overlay");
 
     loginButton.addEventListener("click", function() {
-        // Exibe o popup de login
+
         loginPopup.style.display = "block";
         overlay.style.display = "block";
+        setTimeout(function() {
+            loginPopup.style.opacity = "1";
+            loginPopup.style.transform = "scale(1) translate(-50%, -50%)";
+            overlay.style.opacity = "1";
+        }, 10);
     });
 
     closeModalButton.addEventListener("click", function() {
         // Esconde o popup de login
-        loginPopup.style.display = "none";
-        overlay.style.display = "none";
+        loginPopup.style.opacity = "0";
+        loginPopup.style.transform = "scale(0.8) translate(-50%, -50%)";
+        overlay.style.opacity = "0";
+        setTimeout(function() {
+            loginPopup.style.display = "none";
+            overlay.style.display = "none";
+        }, 300);
     });
 
     overlay.addEventListener("click", function() {
-        loginPopup.style.display = "none";
-        overlay.style.display = "none";
+        loginPopup.style.opacity = "0";
+        loginPopup.style.transform = "scale(0.8) translate(-50%, -50%)";
+        overlay.style.opacity = "0";
+        setTimeout(function() {
+            loginPopup.style.display = "none";
+            overlay.style.display = "none";
+        }, 300);
     });
 });
